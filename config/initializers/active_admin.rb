16e473a16cab6cfdb748343ceeb291a8d0ea8c27
faisyfizz <faisalfiaz300@gmail.com>
2015-12-16 20:21:53 +0500

    cahnged active admin namespace

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -35,6 +35,7 @@ ActiveAdmin.setup do |config|
   #
   # Default:
   # config.default_namespace = :admin
+  config.default_namespace = :activeadmin
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
